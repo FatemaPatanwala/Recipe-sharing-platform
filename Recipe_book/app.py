9a7b0e648052d1f72d4ed872ac82f5015e2316d3
Fatema <patanwalafatema2003@gmail.com>
2024-08-11 11:03:13 +0530

    Updated app.py

--- Recipe_book/app.py
+++
@@ -1,4 +0,0 @@
-from Recipe_book import app
-
-if __name__ == '__main__':
-    app.run(debug=True)
---
+++ app.py
@@ -0,0 +1,4 @@
+from Recipe_book import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
